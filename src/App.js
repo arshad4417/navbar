c3644999a701587d5b62fede7ccbbd3c1d243f6e
arshad4417 <arshadansari4417.com>
2023-11-18 08:14:17 +0530

    18_Nov Scroll Animation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Container from './components/Container'
 
 const App = () => {
+  let arr =[];
+  for(let i=1;i<=100;i++){
+    arr.push(i);
+  }
   return (
     <>
       <Navbar/>
+      {arr.map((value,index)=> {
+        return <Container/>;
+      })};
     </>
   )
 }
